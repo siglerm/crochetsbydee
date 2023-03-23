c5f2bfa7de2dadd3902e7e543126677e58aa119c
Matt <siglerm37@gmail.com>
2023-03-22 19:40:42 -0700

    Created Initial design of home page and added base css

---
+++ crochetsbydee/src/pages/_document.tsx
@@ -0,0 +1,22 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Baloo+Bhaijaan+2&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Baloo+Bhaijaan+2:wght@700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
